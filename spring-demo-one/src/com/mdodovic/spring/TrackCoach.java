07df18cdf6adb2f76130e25274f0ed622b001b9b
mdodovic <matija.dodovic@gmail.com>
2021-10-14 09:37:00 +0200

    add no-arg constructor

--- spring-demo-one/src/com/mdodovic/spring/TrackCoach.java
+++ spring-demo-one/src/com/mdodovic/spring/TrackCoach.java
@@ -8,6 +8,10 @@ public class TrackCoach implements Coach {
 		this.fortuneService = fortuneService;
 	}
 	
+	public TrackCoach() {
+
+	}
+
 	@Override
 	public String getDailyWorkout() {
 		return "Run a hard 5k";
