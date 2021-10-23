b376c72041df94165564ceb4975d5185360c6e33
mdodovic <matija.dodovic@gmail.com>
2021-10-24 00:08:04 +0200

    update CustomerController to use CustomerDAO

--- web-customer-tracker/src/com/mdodovic/controller/CustomerController.java
+++ web-customer-tracker/src/com/mdodovic/controller/CustomerController.java
@@ -1,15 +1,28 @@
 package com.mdodovic.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.mdodovic.dao.CustomerDAO;
+import com.mdodovic.entity.Customer;
+
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
-
+	
+	@Autowired
+	private CustomerDAO customerDAO;
+	
 	@RequestMapping("/list")
-	public String listCustomers(Model theModel) {
+	public String listCustomers(Model model) {
+		
+		List<Customer> allCustomers = customerDAO.getCustomers();
+		
+		model.addAttribute("customers", allCustomers);
 		
 		return "list-customers";
 	}
