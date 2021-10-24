cf00082aa9b0719057fc12fc4eb2298d274a6b8e
mdodovic <matija.dodovic@gmail.com>
2021-10-24 22:57:49 +0200

    save customer to database

--- web-customer-tracker/src/com/mdodovic/dao/CustomerDAO.java
+++ web-customer-tracker/src/com/mdodovic/dao/CustomerDAO.java
@@ -7,5 +7,7 @@ import com.mdodovic.entity.Customer;
 public interface CustomerDAO {
 
 	public List<Customer> getCustomers();
+
+	public void saveCustomer(Customer customer);
 	
 }
--- web-customer-tracker/src/com/mdodovic/service/CustomerService.java
+++ web-customer-tracker/src/com/mdodovic/service/CustomerService.java
@@ -7,5 +7,7 @@ import com.mdodovic.entity.Customer;
 public interface CustomerService {
 
 	public List<Customer> getCustomers();
+
+	public void saveCustomer(Customer customer);
 	
 }
