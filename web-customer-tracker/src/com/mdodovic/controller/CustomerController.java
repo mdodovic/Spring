545c8b222b6b1c3b6b29ba502b8d8bbb32bffaab
mdodovic <matija.dodovic@gmail.com>
2021-10-24 22:47:30 +0200

    bind customer model to form

--- web-customer-tracker/src/com/mdodovic/controller/CustomerController.java
+++ web-customer-tracker/src/com/mdodovic/controller/CustomerController.java
@@ -30,6 +30,11 @@ public class CustomerController {
 	
 	@GetMapping("/showFormForAdd")
 	public String showFormForAdd(Model model) {
+		
+		Customer customer = new Customer();
+		
+		model.addAttribute("customer", customer);
+		
 		return "customer-form";
 	}
 	
