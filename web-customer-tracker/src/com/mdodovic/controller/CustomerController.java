9444bcf04ffe17ede91b36014d6b31bcc42c219e
mdodovic <matija.dodovic@gmail.com>
2021-10-24 22:38:23 +0200

    redirect add request to customer-form.jsp

--- web-customer-tracker/src/com/mdodovic/controller/CustomerController.java
+++ web-customer-tracker/src/com/mdodovic/controller/CustomerController.java
@@ -28,4 +28,9 @@ public class CustomerController {
 		return "list-customers";
 	}
 	
+	@GetMapping("/showFormForAdd")
+	public String showFormForAdd(Model model) {
+		return "customer-form";
+	}
+	
 }
