7470c00a0e07199020e851000a319fbc50743ce2
mdodovic <matija.dodovic@gmail.com>
2021-10-25 09:29:55 +0200

    create empty maven project

---
+++ maven-demo-one/src/main/java/com/mdodovic/maven_demo_one/App.java
@@ -0,0 +1,13 @@
+package com.mdodovic.maven_demo_one;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
