bd7770f59c0e270d3e9e457f72b2278d57f7f7d3
mdodovic <matija.dodovic@gmail.com>
2021-10-29 12:41:01 +0200

    remove unnecessary imports

--- employee-management/src/main/java/com/mdodovic/employeemanagement/rest/EmployeeRestController.java
+++ employee-management/src/main/java/com/mdodovic/employeemanagement/rest/EmployeeRestController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.mdodovic.employeemanagement.dao.EmployeeDAO;
 import com.mdodovic.employeemanagement.entity.Employee;
 import com.mdodovic.employeemanagement.service.EmployeeService;
 
